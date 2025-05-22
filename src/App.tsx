851e480db5c70701dc4462ccbf64075b170778bb
root <root@DESKTOP-M70RUTN.>
2025-05-22 15:49:06 +0000

    navbar update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Home from "./components/Home";
 import Nav from "./components/Nav";
-
+import {Routes, Route} from "react-router-dom";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
 const App = () => {
   return (
     <div className="w-screen h-screen bg-[#0B0D11] cursor-default">
       <Nav/>
+        <Routes>
+          <Route path="/home" element={<Home/>} />
+          <Route path="/project" element={<Project/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
     </div>
   )
 }
