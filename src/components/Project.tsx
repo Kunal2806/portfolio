9c097972ff20da57397c6787e3be3ada4e6e96f7
root <root@DESKTOP-M70RUTN.>
2025-05-23 14:58:23 +0000

    project page update

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,33 @@
 function Project() {
+
+    const projectList = [
+        
+    ]
     return (
-        <div>Projects</div>
+        <div 
+            className="w-full not-first:flex justify-center"    
+        >
+            <div
+                style={{height: "calc(100vh - 280px"}}
+                className="w-full flex flex-col justify-center items-center text-white font-Lato text-center"
+            >
+                <h1 
+                    className="text-2xl"
+                > Welcome to
+                </h1>
+                <h1 
+                    className="text-4xl font-Jaro border-2 px-10 rounded-full bg-[#d1d3d4a9] text-[#000] py-1"
+                > REPOSITORY    
+                </h1>
+                <p className="text-xs m-2 text-[#d1d3d4a9]">Each project reflects my journey, experiments,<br/> and learning experiences.</p>
+            </div>
+
+            <div
+                className="grid grid-cols-3"
+            >
+
+            </div>
+        </div>
     )
 }
 
