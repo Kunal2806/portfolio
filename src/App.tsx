a58109410ac86e4d103f01cffb340089387a79ea
root <root@DESKTOP-M70RUTN.>
2025-05-23 19:03:33 +0000

    project update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Project from "./components/Project";
 import Contact from "./components/Contact";
 const App = () => {
   return (
-    <div className="w-screen h-screen bg-[#0B0D11] cursor-default">
+    <div className="w-screen h-screen overflow-x-hidden bg-[#0B0D11] cursor-default">
       <Nav/>
         <Routes>
           <Route path="/home" element={<Home/>} />
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,11 +1,22 @@
 function Project() {
 
     const projectList = [
-        
+        {
+            src:"./projectImg/chat.png",
+            disc:"A chat website that does not store any type of user data.All data is deleted when the user disconnects from the server."
+        },
+        {
+            src:"./projectImg/todo.png",
+            disc:"A full-stack To-Do application that allows users to manage tasks with secure authentication"
+        },
+        {
+            src:"./projectImg/portfolio.png",
+            disc:"A responsive portfolio showcasing my projects and skills with a focus on clean design and smooth user experience."
+        },
     ]
     return (
         <div 
-            className="w-full not-first:flex justify-center"    
+            className="w-full flex flex-col justify-center"    
         >
             <div
                 style={{height: "calc(100vh - 280px"}}
@@ -23,9 +34,22 @@ function Project() {
             </div>
 
             <div
-                className="grid grid-cols-3"
+                className="grid grid-cols-3 place-items-center gap-10 mx-30 m-2"
             >
-
+                {
+                    projectList.map((_prop,i)=>(
+                        <div key={i}
+                            className="border-2 w-[400px] h-[500px] border-[#d1d3d4a9] hover:border-white"
+                        >
+                            <div className="p-2 h-[250px] flex items-center ">
+                                <img className="rounded-2xl" src={_prop.src} alt="project_ss" />
+                            </div>
+                            <div className="bg-[#15161A] h-[240px] text-center p-10 m-2">
+                                <p className="text-white font-Lato text-xl">{_prop.disc}</p>
+                            </div>                           
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
