12ed794cfe6af5bcc875725d321493f9c0c3a44c
Kunal Goswami <kunalgoswami.2806@gmail.com>
2025-08-23 21:36:00 +0530

    Updated App.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,76 +1,60 @@
-import { useState } from "react";
-import { Link, useLocation} from "react-router-dom";
+import {motion} from "motion/react";
+import { useRef } from "react";
 
-const Nav = () => { 
-    const [hoverLogo, setHoverLogo] = useState({url: "/images/logo.svg", text: "Kunal Goswami"});
-    const {pathname} = useLocation();
+function Home() {
+
+    const constraintsRefs = useRef(null);
+    const skills =[
+        "./images/react.svg",
+        "./images/nodejs.svg",
+        "./images/honojs.svg",
+        "./images/expressjs.svg",
+        "./images/figma.svg",
+        "./images/css.svg",
+        "./images/html.svg",
+        "./images/typescript.svg",
+        "./images/tailwind.svg",
+        "./images/javascript.svg",
+        "./images/github.svg",
+        "./images/git.svg",
+        "./images/mongo.svg",
+        "./images/sql.svg",
+    ]
     return (
-        <div className="w-full h-[100px]">
-            <div className={ pathname == "/home"?`px-30 w-full h-[80px] flex justify-between items-center border-b-4 border-[#3b82f6] transition duration-800 ease-in-out` :
-                pathname == "/project"? `px-30 w-full h-[80px] flex justify-between items-center border-b-4 border-[#d1d3d4a9] transition duration-800 ease-in-out` :
-                "px-30 w-full h-[80px] flex justify-between items-center border-b-4 border-[#70FFAF] transition duration-800 ease-in-out"
-            }>
-                <div className="flex items-center cursor-pointer">
-                    <img
-                        src={hoverLogo.url}
-                        alt="Kunal Goswami logo"
-                        className="size-[42px]"
-                        onMouseEnter={() => setHoverLogo({ url: "/images/logo1.svg", text: "KG 2806" })}
-                        onMouseLeave={() => setHoverLogo({ url: "/images/logo.svg", text: "Kunal Goswami" })}
-                    />
-                    <h1 className="text-white font-Jaro text-4xl">{hoverLogo.text}</h1>
-                </div>
-                <a
-                    href="https://drive.google.com/file/d/1tTz8dA1DSoSbfQQIOo0Q_dLRz_9VhzWt/view?usp=sharing"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    aria-label="Download Kunal Goswami's Resume"
-                >
-                    <div className={ pathname == '/home' ? `font-Lato text-black pr-6 py-2 pl-7 border-2 transition duration-800 ease-in-out  rounded-xl text-xl flex items-center justify-center bg-[#3b82f6] hover:bg-[#3b83f6e1]` :
-                        pathname == '/project' ? `font-Lato text-black pr-6 py-2 pl-7 border-2 transition duration-800 ease-in-out  rounded-xl text-xl flex items-center justify-center bg-[#d1d3d4a9] hover:bg-[#ffffff]` :
-                        `font-Lato text-black pr-6 py-2 pl-7 border-2 transition duration-800 ease-in-out  rounded-xl text-xl flex items-center justify-center bg-[#70FFAF] hover:bg-[#70ffaed2]`
-                    }>
-                        Resume {'>'}
-                    </div>
-                </a>
+        <div className="w-full h-screen flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center text-white">
+                <h1 className="font-Lato text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center">Hi, I'm Kunal Goswami</h1>
+                <h2 className="font-Lato text-lg sm:text-xl md:text-2xl lg:text-3xl mt-2">FULL STACK</h2>
+                <hr className="bg-[#3b82f6] border-[#3b82f6] h-[2px] sm:h-[3px] w-32 sm:w-48 md:w-64 m-1 rounded-full" />
+                <h2 className="font-Jaro text-xl sm:text-2xl md:text-3xl lg:text-4xl">Web Developer</h2>
+                <p className="text-sm sm:text-base md:text-lg font-Lato mt-4 text-center max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl px-4">
+                    Passionate about creating innovative web solutions with modern technologies. 
+                    Explore my professional background, achievements, and projects.
+                </p>
             </div>
-            <div className="pl-60 w-full flex justify-between items-center text-sm -translate-2">
-                <div className="flex text-white gap-4 text-center">
-                    <div className="flex flex-col items-center">
-                        <svg className={pathname == "/home" ? "transition duration-800 ease-in-out" : "invisible"} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#3b82f6"><path d="M480-360 280-560h400L480-360Z"/></svg>                    
-                        <Link to="/home" 
-                            className= {pathname =="/home" ?" border px-5 py-1 pt-2 font-Lato rounded-md flex flex-col items-center justify-center transition duration-800 ease-in-out  border-[#3b82f6] hover:border-[#3b83f6e1]":
-                                "border px-5 py-1 pt-2 font-Lato rounded-md flex flex-col items-center justify-center transition duration-800 ease-in-out"}  >
-                            HOME
-                        </Link>
-                    </div>
-                    <div className="flex flex-col items-center">
-                        <svg className={pathname == "/project" ? "transition duration-800 ease-in-out" : "invisible"}  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#D1D3D4"><path d="M480-360 280-560h400L480-360Z"/></svg>
-                        <Link to="/project" 
-                            className= {pathname =="/project" ?"border px-5 py-1 pt-2 font-Lato rounded-md flex flex-col items-center justify-center transition duration-800 ease-in-out  border-[#d1d3d4a9] hover:border-[#ffffff]":
-                                "border px-5 py-1 pt-2 font-Lato rounded-md flex flex-col items-center justify-center transition duration-800 ease-in-out"}  >
-                            PROJECTS
-                        </Link>
-                    </div>
-                    <div className="flex flex-col items-center">
-                        <svg className={pathname == "/contact" ? "transition duration-800 ease-in-out" : "invisible"}  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#70FFAF"><path d="M480-360 280-560h400L480-360Z"/></svg>
-                        <Link to="/contact"
-                            className= {pathname =="/contact" ?"border px-5 py-1 pt-2 font-Lato rounded-md flex flex-col items-center justify-center transition duration-800 ease-in-out  border-[#70FFAF] hover:border-[#70ffaed2]":
-                                "border px-5 py-1 pt-2 font-Lato rounded-md flex flex-col items-center justify-center transition duration-800 ease-in-out"}  >
-                                CONTACT
-                        </Link>
-                    </div>
-                </div>
-                <div className="mr-80 flex flex-col items-center">
-                    <div className={pathname == '/home' ? `border px-5 py-1 pt-2 font-Lato rounded-md flex flex-col items-center justify-center transition duration-800 ease-in-out  bg-[#3b82f6] hover:bg-[#3b83f6e1]` :
-                        pathname == '/project' ? `border px-5 py-1 pt-2 font-Lato rounded-md flex flex-col items-center justify-center transition duration-800 ease-in-out  bg-[#d1d3d4a9] hover:bg-[#ffffff]` :
-                        `border px-5 py-1 pt-2 font-Lato rounded-md flex flex-col items-center justify-center transition duration-800 ease-in-out  bg-[#70FFAF] hover:bg-[#70ffaed2]`}>
-                        PORTFOLIO
-                    </div>
-                </div>
+            <div ref={constraintsRefs} className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-5 h-full w-full place-items-center gap-2 sm:gap-4 p-4">
+                {
+                    skills.map((_src, i)=> {
+                        if(i==7) {
+                            return [
+                                <div key="empty" className="size-8 sm:size-12 md:size-16"/>,
+                                <motion.img className="size-8 sm:size-12 md:size-16 z-50 cursor-grab" key={i} src={_src} alt="skill" 
+                                    drag
+                                    dragConstraints={constraintsRefs}
+                                />
+                            ] ;
+                        }
+                        return(
+                            <motion.img className="size-8 sm:size-12 md:size-16 z-50 cursor-grab" key={i} src={_src} alt="skill" 
+                                drag
+                                dragConstraints={constraintsRefs}
+                            /> 
+                        )
+                    })
+                }
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Nav;
+export default Home;
